dc2bf1bd7823c5d668eda8e3a602c6d8b13ac585
Elaine <elaine.cadman@gmail.com>
2019-02-11 21:42:49 -0800

    Updates

---
+++ precache-manifest.7f874966830f2825fa6f49341f55a647.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "cba96714fee0430c139f",
+    "url": "/clicky-game/static/js/main.cba96714.chunk.js"
+  },
+  {
+    "revision": "8049a5204cbae57fc29f",
+    "url": "/clicky-game/static/js/1.8049a520.chunk.js"
+  },
+  {
+    "revision": "cba96714fee0430c139f",
+    "url": "/clicky-game/static/css/main.77c18b7b.chunk.css"
+  },
+  {
+    "revision": "2098edc2a34b12241941b004917fd9b1",
+    "url": "/clicky-game/index.html"
+  }
+];
